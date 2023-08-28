3475528539720db61298cd0ce1ccd8a8927a5297
sunilkr09 <93273505+sunilkr09@users.noreply.github.com>
2023-08-29 02:58:14 +0530

    Solved - FindAndFill

--- src/main/java/FindAndFill.java
+++ src/main/java/FindAndFill.java
@@ -1,18 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import utils.Browser;
 
 import static utils.BrowserWebDriver.openUrl;
 
 public class FindAndFill {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = openUrl(Browser.CHROME, "https://obstaclecourse.tricentis.com/Obstacles/73590");
-        Thread.sleep(5000);
-        WebElement pass = driver.findElement(By.id("pass"));
-              pass.clear();
-              pass.sendKeys("ABC");
+    public static void main(String[] args) {
+        WebDriver driver = openUrl(Browser.FIREFOX, "https://obstaclecourse.tricentis.com/Obstacles/73590");
+
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("actual")).sendKeys("ABC");
         driver.findElement(By.id("sample")).click();
+
         try {
             Thread.sleep(5000);
             if (driver.findElement(By.xpath("//h2[text()='Good job!']")).isDisplayed())
@@ -20,7 +19,7 @@ public class FindAndFill {
         } catch (Exception e) {
             System.out.println("Problem Not Solved");
         } finally {
-            //driver.quit();
+            driver.quit();
         }
     }
 }
