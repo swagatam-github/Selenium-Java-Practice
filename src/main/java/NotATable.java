78b0c89c74c17b27d142d105ba8fad7999fdf170
sunilkr09 <sk2321996@gmail.com>
2023-08-28 10:06:23 +0530

    Solved - NotATable (New)

--- src/main/java/NotATable.java
+++ src/main/java/NotATable.java
@@ -1,24 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import utils.Browser;
-
-import java.util.List;
 
 import static utils.BrowserWebDriver.openUrl;
 
 public class NotATable {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         WebDriver driver = openUrl(Browser.CHROME, "https://obstaclecourse.tricentis.com/Obstacles/64161");
 
         driver.findElement(By.id("generate")).click();
-        Thread.sleep(5000);
-        List<WebElement> table = driver.findElements(By.xpath("//div[@class='row propertyGrid']"));
-        String orderId = "";
-        for (int i = 1; i <= table.size(); i++) {
-            if (driver.findElement(By.xpath("(//div[@class='col-md-4 bg-info border'])[" + i + "]")).getText().equals("order id"))
-                orderId = driver.findElement(By.xpath("(//div[@class='col-md-4 border'])[" + i + "]")).getText();
-        }
+        String orderId = driver
+                .findElement(By.xpath("//div[text()='order id']/following-sibling::div"))
+                .getText().trim();
         driver.findElement(By.id("offerId")).sendKeys(orderId);
 
         try {
