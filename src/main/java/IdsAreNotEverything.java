9db46f73ae549867c44b61c9aa9954b901ffcba1
Swagatam Ghosh <steamswagatam@gmail.com>
2023-08-29 20:14:59 +0530

    Chanages - IdsAreNotEverything

--- src/main/java/IdsAreNotEverything.java
+++ src/main/java/IdsAreNotEverything.java
@@ -8,16 +8,18 @@ import static utils.BrowserWebDriver.openUrl;
 
 public class IdsAreNotEverything {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = openUrl(Browser.CHROME, "https://obstaclecourse.tricentis.com/Obstacles/22505/retry");
+        WebDriver driver = openUrl(Browser.CHROME, "https://obstaclecourse.tricentis.com/Obstacles/22505");
         Thread.sleep(5000);
         WebElement ClickMe = driver.findElement(By.xpath("//a[text()='Click me!']"));
         ClickMe.click();
-        Thread.sleep(5000);
-        if (driver.findElement(By.xpath("//h2[text()='Good job!']")).isDisplayed())
-            System.out.println("Problem Solved");
-        else
+        try {
+            Thread.sleep(5000);
+            if (driver.findElement(By.xpath("//h2[text()='Good job!']")).isDisplayed())
+                System.out.println("Problem Solved");
+        } catch (Exception e) {
             System.out.println("Problem Not Solved");
-        driver.quit();
+        } finally {
+            driver.quit();
+        }
     }
-
 }
