49661b901b09df6408d2ae822d4d15eb82f072e4
sunilkr09 <sk2321996@gmail.com>
2023-09-19 16:22:25 +0530

    Added clickElementByOffSet Method

--- src/main/java/utils/ElementOperations.java
+++ src/main/java/utils/ElementOperations.java
@@ -2,6 +2,10 @@ package utils;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.function.BiFunction;
 
 public class ElementOperations {
     public static void staticWait(long timeInSeconds) {
@@ -10,6 +14,15 @@ public class ElementOperations {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void clickElementByOffSet(WebDriver driver, WebElement element, int widthOffsetPercent, int heightOffsetPercent) {
+        BiFunction<Integer, Integer, Integer> calculateOffset = (Integer a, Integer percent) -> (int) ((a * -0.5) + (a * (percent / 100.0)));
+
+        int widthOffset = calculateOffset.apply(element.getRect().getDimension().getWidth(), widthOffsetPercent);
+        int heightOffset = calculateOffset.apply(element.getRect().getDimension().getHeight(), heightOffsetPercent);
+
+        new Actions(driver).moveToElement(element, widthOffset, heightOffset).click().perform();
     }
 
     public static void scrollByAmount(WebDriver driver, int dX, int dY) {
