feeeb6ffba8e06542f75613090f2c53d1ba80fd1
sunilkr09 <sk2321996@gmail.com>
2024-10-14 22:34:07 +0530

    Added Enum for DesktopBrowsers

---
+++ src/main/java/base/DesktopBrowserType.java
@@ -0,0 +1,9 @@
+package base;
+
+public interface DesktopBrowserType {
+    String CHROME = "chrome";
+    String EDGE = "edge";
+    String FIREFOX = "firefox";
+    String EDGE_HEADLESS = EDGE.concat("_headless");
+    String CHROME_HEADLESS = CHROME.concat("_headless");
+}
